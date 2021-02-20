fb06662e95338f858877c7cea6ed71cc2143a08c
Harish <hareeshankar@gmail.com>
2021-02-20 18:03:40 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"603100ad7defbd001eed3e2f",
+				"603100ad7defbd001eed3e31"
+			]
+		},
+		"603100ad7defbd001eed3e2f": {
+			"id": "603100ad7defbd001eed3e2f",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"603100ad7defbd001eed3e31": {
+			"id": "603100ad7defbd001eed3e31",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
